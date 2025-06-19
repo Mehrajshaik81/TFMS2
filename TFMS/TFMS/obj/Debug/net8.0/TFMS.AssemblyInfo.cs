7c4025b926c59c131aa9eabeb8c0518bc6b629bd
Keerthivarmagandham60 <KeerthiVarma.Gandham@cognizant.com>
2025-06-19 11:03:28 +0530

    commit-2

--- TFMS/TFMS/obj/Debug/net8.0/TFMS.AssemblyInfo.cs
+++ TFMS/TFMS/obj/Debug/net8.0/TFMS.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d75ae0c7a468ccdd2f1b7afa1d869a7864e21641")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17ef6cc19058b7121f4f247b59f25753ba75ac36")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
